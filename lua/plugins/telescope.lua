a5884270b41f3e49a18e74c00bda67e229eb7cdd
Neetin Nagap <nnagap72@gmail.com>
2025-05-05 12:08:09 +0530

    Ignoring node_modules

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -3,6 +3,7 @@ return {
   tag = '0.1.8',
   dependencies = { 'nvim-lua/plenary.nvim' },
   config = function()
+    require('telescope').setup{ defaults = { file_ignore_patterns = {"node_modules"} } }
     local builtin = require("telescope.builtin")
     vim.keymap.set('n', '<C-p>', builtin.find_files, {})
     vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
