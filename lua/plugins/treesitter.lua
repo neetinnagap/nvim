32e7d72e66c6006bdeda43a206034028cdfecb30
Neetin Nagap <nnagap72@gmail.com>
2025-05-05 11:59:26 +0530

    Adding linter and formatter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,12 +1,13 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  built = ":TSupdate",
-  config = function()
-    local config = require("nvim-treesitter.configs")
-    config.setup({
-      ensure_installed = {"lua", "java", "javascript", "typescript"},
-      highlight = { enabled = true },
-      indent = { enabled = true },
-    })
-  end
+	"nvim-treesitter/nvim-treesitter",
+	built = ":TSupdate",
+	config = function()
+		local config = require("nvim-treesitter.configs")
+		config.setup({
+			auto_intall = true,
+			-- ensure_installed = {"lua", "java", "javascript", "typescript"},
+			highlight = { enabled = true },
+			indent = { enabled = true },
+		})
+	end,
 }
